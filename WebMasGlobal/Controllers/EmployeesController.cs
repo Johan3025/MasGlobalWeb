9b187224871eeadffa9e99c33e81f6223959bfb6
Johan Ospina Nunez <Aospina@intergrupo.com>
2019-11-17 13:02:18 -0500

    primer subida

---
+++ WebMasGlobal/Controllers/EmployeesController.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using WebMasGlobal.Models;
+using System;
+
+namespace WebMasGlobal.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly WebMasGlobalContext context;
+
+        public EmployeesController(WebMasGlobalContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            return View(await context.Employees.ToListAsync());
+        }
+
+        // GET: Employees
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+
+        // GET: Employees/Details/5
+        [HttpPost]
+        public async Task<IActionResult> Details(int? id)
+        {
+
+            try
+            {
+                List<Employees> reservationList = new List<Employees>();
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.GetAsync("https://localhost:44311/api/Employes?id=" + id))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        reservationList = JsonConvert.DeserializeObject<List<Employees>>(apiResponse);
+                    }
+                }
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //// POST: Employees/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("Id,Name,ContractTypeName,RoleId,RoleName,RoleDescription,HourlySalary,MonthlySalary,Total")] Employees employees)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(employees);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(employees);
+        //}
+
+        //// GET: Employees/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var employees = await _context.Employees.FindAsync(id);
+        //    if (employees == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(employees);
+        //}
+
+        //// POST: Employees/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("Id,Name,ContractTypeName,RoleId,RoleName,RoleDescription,HourlySalary,MonthlySalary,Total")] Employees employees)
+        //{
+        //    if (id != employees.Id)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(employees);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!EmployeesExists(employees.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(employees);
+        //}
+
+        //// GET: Employees/Delete/5
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var employees = await _context.Employees
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (employees == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(employees);
+        //}
+
+        //// POST: Employees/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var employees = await _context.Employees.FindAsync(id);
+        //    _context.Employees.Remove(employees);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+        private bool EmployeesExists(int id)
+        {
+            return context.Employees.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebMasGlobal/Data/WebMasGlobalContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebMasGlobal.Models;
+
+namespace WebMasGlobal.Models
+{
+    public class WebMasGlobalContext : DbContext
+    {
+        public WebMasGlobalContext (DbContextOptions<WebMasGlobalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employees> Employees { get; set; }
+
+        public DbSet<WebMasGlobal.Models.Estudiante> Estudiante { get; set; }
+    }
+}
